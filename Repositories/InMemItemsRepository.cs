64c0b911bf1bdc5e78242c08b3a2f14bc7626c61
Sharizan Mohd Redzuan <ijanzz5621@gmail.com>
2021-08-16 08:53:46 +0800

    add missing files

---
+++ Repositories/InMemItemsRepository.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Catalog.Entities;
+
+namespace Catalog.Repositories
+{
+    public class InMemItemsRepository : IItemsRepository
+    {
+        private readonly List<Item> _items = new()
+        {
+            new Item { Id = Guid.NewGuid(), Name = "Potion", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreatedDate = DateTimeOffset.UtcNow },
+        };
+
+        public async Task<IEnumerable<Item>> GetItemsAsync()
+        {
+            return await Task.FromResult(_items);
+        }
+
+        public async Task<Item> GetItemAsync(Guid id)
+        {
+            var item = _items.Where(item => item.Id == id).SingleOrDefault();
+            return await Task.FromResult(item);
+        }
+
+        public async Task CreateItemAsync(Item item)
+        {
+            _items.Add(item);
+            await Task.CompletedTask;
+        }
+
+        public async Task UpdateItemAsync(Item item)
+        {
+            var index = _items.FindIndex(existingItem => existingItem.Id == item.Id);
+            _items[index] = item;
+
+            await Task.CompletedTask;
+        }
+
+        public async Task DeleteItemAsync(Guid id)
+        {
+            var index = _items.FindIndex(existingItem => existingItem.Id == id);
+            _items.RemoveAt(index);
+
+            await Task.CompletedTask;
+        }
+    }
+}
---
+++ Repositories/MongoDbItemsRepository.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Catalog.Entities;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Catalog.Repositories
+{
+    public class MongoDbItemsRepository : IItemsRepository
+    {
+        private const string _databaseName = "catalog";
+        private const string _collectionName = "items";
+        private readonly IMongoCollection<Item> _itemsCollection;
+        private readonly FilterDefinitionBuilder<Item> _filterBuilder = Builders<Item>.Filter;
+
+        public MongoDbItemsRepository(IMongoClient mongoClient)
+        {
+            IMongoDatabase database = mongoClient.GetDatabase(_databaseName);
+            _itemsCollection = database.GetCollection<Item>(_collectionName);
+        }
+        public async Task CreateItemAsync(Item item)
+        {
+            await _itemsCollection.InsertOneAsync(item);
+        }
+
+        public async Task DeleteItemAsync(Guid id)
+        {
+            var filter = _filterBuilder.Eq(item => item.Id, id);
+            await _itemsCollection.DeleteOneAsync(filter);
+        }
+
+        public async Task<Item> GetItemAsync(Guid id)
+        {
+            var filter = _filterBuilder.Eq(item => item.Id, id);
+            return await _itemsCollection.Find(filter).SingleOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Item>> GetItemsAsync()
+        {
+            return await _itemsCollection.Find(new BsonDocument()).ToListAsync();
+        }
+
+        public async Task UpdateItemAsync(Item item)
+        {
+            var filter = _filterBuilder.Eq(existingItem => existingItem.Id, item.Id);
+            await _itemsCollection.ReplaceOneAsync(filter, item);
+        }
+    }
+}
